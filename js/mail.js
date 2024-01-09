ae43989b6b51fd3536470d2750e4741df9bc3013
poloztech <poloztech.ug@gmail.com>
2024-01-09 16:21:25 +0300

    Commit

---
+++ js/mail.js
@@ -0,0 +1,22 @@
+function sendEmail(){
+    let email = document.getElementById('email').value;
+let first_name = document.getElementById('first_name').value;
+let last_name = document.getElementById('last_name').value;
+let phone = document.getElementById('phone').value;
+let select_service = document.getElementById('select_service').value;
+let comments = document.getElementById('comments').value;
+let form = document.getElementById('contactform');
+
+emailjs.send("service_nzulu27","template_cr8e6bm",{
+    email,
+    first_name,
+    last_name,
+    phone,
+    select_service,
+    comments,
+    message
+})
+form.reset();
+}
+
+
